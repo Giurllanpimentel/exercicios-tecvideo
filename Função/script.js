c31eae5fcfc0a3d207590e519679457fe9cc75b4
Giurllan Pimentel <146568002+Giurllanpimentel@users.noreply.github.com>
2025-03-25 15:56:23 -0300

    Funcao

---
+++ Função/script.js
@@ -0,0 +1,8 @@
+// Neste exercício vamos aprender como criar uma função em JavaScript que soma dois números inteiros.
+
+
+function solveMeFirst(a, b){
+  return a + b
+}
+
+console.log(solveMeFirst(7, 3) );

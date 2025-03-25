1fc06f1015ffd8c2fd2a2c308aea131fdef310de
Giurllan Pimentel <146568002+Giurllanpimentel@users.noreply.github.com>
2025-03-25 11:55:06 -0300

    exercicio 3

---
+++ Exercicio 3/script.js
@@ -0,0 +1,21 @@
+/*EXERCÍCIO 3
+=========================
+Elaborar um programa para uma loja, o qual leia o preço de um produto e informe as opções de pagamento da loja. 
+Calcule e informe o valor para pagamento à vista com 10% de desconto e o valor em 3x.
+EXEMPLO:
+Preço: R$60,00
+À vista: R$54,00
+Ou 3x de: R$20,00
+=========================
+*/
+
+let preço = 60
+let aVista = 10
+let totalAvista = 0
+
+ totalAvista = (preço - (aVista / 100) * preço)
+ console.log(`Preço: R$: ${preço},00`);
+ console.log(`Ávista: R$ ${totalAvista},00`);
+ console.log(`Ou 3x de: R$ ${preço / 3},00`);
+ 
+ 
